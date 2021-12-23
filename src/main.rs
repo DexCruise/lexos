f01916a2b35c950bfc5dfc5cbdfaa21a38cb93f2
DexCruise <dexter.e.cruise@gmail.com>
2021-12-23 14:33:58 -0800

    a

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
+
 #[no_mangle]
-pub extern "C" fn _start() {
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
 
